bf5939b3ad7ad4af24145246961d64f5a0a5c7c8
Marcus Vinícius Torres <44293110+marcustorres02@users.noreply.github.com>
2023-07-25 17:04:49 -0300

    Examples JavaScript

--- Fundamentos/js_e_nodejs/console.js
+++ Fundamentos/js_e_nodejs/console.js
@@ -20,3 +20,9 @@ console.table() para visualizar de forma mais organizada informações tabulares
 console.time() e console.timeEnd() para temporizar período que uma operação de código leva para ser iniciada e concluída;
 console.trace() para exibir a stacktrace de todos os pontos (ou seja, os arquivos chamados) por onde o código executado passou durante a execução.
 */
+
+console.log("deu erro");
+console.error("deu erro");
+
+console.log("deu erro");
+//console.error(new Error("deu erro"));
