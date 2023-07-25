157e1d3f73e9d21dca0bcfc843eea24a90598b61
Marcus Vinícius Torres <44293110+marcustorres02@users.noreply.github.com>
2023-07-25 16:42:55 -0300

    Examples JavaScript

---
+++ Fundamentos/js_e_nodejs/script.js
@@ -0,0 +1,8 @@
+// tipagem dinâmica: Aceita trocar os tipos das variáveis
+// untyped: não precisa declarar o tipo da variável
+
+let minhaVar = 567;
+minhaVar = "texto";
+minhaVar = true;
+
+// O que é o NodeJS: É um interpretador do JavaScript no Backend.
