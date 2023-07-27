8b3459696af6dfe34c482c23e61f1d1e77b5fb80
Marcus Vinícius Torres <44293110+marcustorres02@users.noreply.github.com>
2023-07-27 10:16:41 -0300

    Examples JavaScript

--- Fundamentos/arrays/o-que-sao-arrays/media.js
+++ Fundamentos/arrays/o-que-sao-arrays/media.js
@@ -13,4 +13,4 @@ const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
 console.log(media);
 
-// A propriedade length retorna o tamanho de um Array
+// A propriedade length retorna o tamanho de um Arrayy
