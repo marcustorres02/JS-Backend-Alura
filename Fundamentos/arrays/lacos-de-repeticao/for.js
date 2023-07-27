c2a53734e853901d97ac5868f4ba58debf5520e5
Marcus Vinícius Torres <44293110+marcustorres02@users.noreply.github.com>
2023-07-27 15:47:33 -0300

    Examples JavaScript

---
+++ Fundamentos/arrays/lacos-de-repeticao/for.js
@@ -0,0 +1,11 @@
+const numeros = [100, 200, 300, 400, 500, 600];
+
+// For possui três partes: 1. Inicialização; 2. Condição de parada; 3. Incremento
+
+// Primeira expressão é executada apenas uma vez
+// Segunda expressão é a condição de execução
+// Terceira expressão é executada sempre ao final de um laço
+
+for (let indice = 0; indice < numeros.length; indice++) {
+    console.log(numeros[indice]);
+}
