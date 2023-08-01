85f9fef0efcfc8d3a0e26e99e883dd50b8752e9a
Marcus Vinícius Torres <44293110+marcustorres02@users.noreply.github.com>
2023-08-01 11:40:30 -0300

    Encontrando menor valor em uma lista de objetos não ordenada

--- Algoritmos/pensando-em-algoritmos/livros.js
+++ Algoritmos/pensando-em-algoritmos/livros.js
@@ -1,12 +1,12 @@
-const precosLivros = [25, 15, 30, 50, 45, 20];
+const livros = require('./listaLivros');
 
 let atual = 0;
 let maisBarato = 0;
 
-for (let atual = 0; atual < precosLivros.length; atual++) {
-    if (precosLivros[atual] < precosLivros[maisBarato]) {
+for (let atual = 0; atual < livros.length; atual++) {
+    if (livros[atual].preco < livros[maisBarato].preco) {
         maisBarato = atual;
     }
 }
 
-console.log(`O livro mais barato custa ${precosLivros[maisBarato]}`);
+console.log(`O livro mais barato custa ${livros[maisBarato].preco} e o título é ${livros[maisBarato].titulo}`);
