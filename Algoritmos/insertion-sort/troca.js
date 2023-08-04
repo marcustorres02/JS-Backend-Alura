958c6ff19b490f9d80d66768b68c979aa9275513
Marcus Vinícius Torres <44293110+marcustorres02@users.noreply.github.com>
2023-08-04 15:17:57 -0300

    Algoritmo de troca para ordenação

---
+++ Algoritmos/insertion-sort/troca.js
@@ -0,0 +1,11 @@
+// Função para realizar a troca dos elementos na ordenação
+
+function troca(lista, analise) {
+    let itemAnalise = lista[analise];
+    let itemAnterior = lista[analise - 1];
+        
+    lista[analise] = itemAnterior;
+    lista[analise - 1] = itemAnalise;
+}
+
+module.exports = troca;
