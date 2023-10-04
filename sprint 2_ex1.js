e17eafb81a86cb0680ebfb4447a78ecd55c34b49
Amanda <amanda.varnier@hotmail.com>
2023-10-04 16:45:57 -0300

    update

--- sprint 2_ex1.js
+++ sprint 2_ex1.js
@@ -7,5 +7,3 @@ let endereço = {
 };
    
 console.log(`O usuário mora em ${endereço.cidade} / ${endereço.uf}, no bairro ${endereço.bairro}, na ${endereço.rua}, nº ${endereço.numero}.`);
-
-// primeiro usei o "let" para declarar os objetos e em seguida utilizei o "console.log" para mostrar as propriedades do objeto usando uma string formatada que acessa essas propriedades.
